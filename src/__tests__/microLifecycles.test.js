1c3135002ad8ddb81e19c06fd6b88413a6221609
streamich <vadimsdaleckis@gmail.com>
2018-03-29 00:58:19 +0100

    test: add $update tests

--- src/__tests__/microLifecycles.test.js
+++ src/__tests__/microLifecycles.test.js
@@ -8,6 +8,17 @@ describe('microLifecycles()', () => {
         expect(typeof microLifecycles).toBe('function');
     });
 
+    it('does not create .ref, if not necessary', () => {
+        const $attach = jest.fn();
+        const props = {
+            foo: 'bar'
+        };
+
+        const newProps = microLifecycles(props);
+
+        expect(typeof newProps.ref).toBe('undefined');
+    });
+
     it('$attach', () => {
         const $attach = jest.fn();
         const props = {
@@ -44,5 +55,49 @@ describe('microLifecycles()', () => {
         newProps.ref(el);
 
         expect($attach).toHaveBeenCalledTimes(1);
+    });
+
+    it('$update', () => {
+        const $update = jest.fn();
+        const props = {
+            $update,
+            foo: 'bar'
+        };
+
+        let newProps = microLifecycles(props);
+
+        expect(typeof newProps.ref).toBe('function');
+        expect($update).toHaveBeenCalledTimes(0);
+
+        newProps.ref(null);
+
+        expect($update).toHaveBeenCalledTimes(0);
+
+        const el = {};
+
+        newProps.ref(el);
+
+        expect($update).toHaveBeenCalledTimes(0);
+
+        newProps.ref(el);
+
+        expect($update).toHaveBeenCalledTimes(1);
+        expect($update.mock.calls[0][0]).toBe(el);
+        expect($update.mock.calls[0][1]).toMatchObject({
+            foo: 'bar'
+        });
+
+        newProps = microLifecycles({
+            $update,
+            foo: 'bar2'
+        });
+        newProps.ref(null);
+        newProps.ref(el);
+
+        expect($update).toHaveBeenCalledTimes(2);
+        expect($update.mock.calls[1][0]).toBe(el);
+        expect($update.mock.calls[1][1]).toMatchObject({
+            foo: 'bar2'
+        });
     });
 });
