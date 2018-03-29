653d10ac701441924c129214e2753ac393817853
streamich <vadimsdaleckis@gmail.com>
2018-03-29 01:01:53 +0100

    feat: export index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import microLifecycles from './microLifecycles';
 
-const createHyperscript = (h) => {
+export const createHyperscript = (h) => {
     return (...args) => {
         args[1] = microLifecycles(args[1] || {});
 
         return h(...args);
     };
 };
-
-export default createHyperscript;
