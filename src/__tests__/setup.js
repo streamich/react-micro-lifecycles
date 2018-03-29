89ade71192df9b37c9854eb297d6f018550943cc
streamich <streamich@gmail.com>
2018-03-29 23:51:23 +0100

    test: add createHyperscriptStable() tests

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -1,1 +1,10 @@
+const {configure} = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+
 process.env.NODE_ENV = 'production';
+
+configure({
+  adapter: new Adapter()
+});
+
+global.requestAnimationFrame = window.requestAnimationFrame = (callback) => setTimeout(callback, 17);
